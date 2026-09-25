3a3abcda19252b9303c5e8e97b58d8c54609e039
Prashant471-cmd <deujaprashant21@gmail.com>
2026-09-25 22:45:28 +0545

    Fix Profile page build by adding designer file and registering in csproj

---
+++ AviLearn.Web/Profile.aspx.designer.cs
@@ -0,0 +1,12 @@
+namespace AviLearn.Web
+{
+    public partial class Profile
+    {
+        protected global::System.Web.UI.WebControls.Label lblInitial;
+        protected global::System.Web.UI.WebControls.Label lblName;
+        protected global::System.Web.UI.WebControls.Label lblRole;
+        protected global::System.Web.UI.WebControls.Label lblEmail;
+        protected global::System.Web.UI.WebControls.Label lblXP;
+        protected global::System.Web.UI.WebControls.Label lblJoinYear;
+    }
+}
